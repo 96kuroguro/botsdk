e94dafe509c120c3d090296c881c66bbc86927de
96kuroguro <96kuroguro@gmail.com>
2017-08-29 13:47:45 +0900

    commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('me', 'ApiController@me');
+Route::get('update', 'ApiController@update');
+Route::get('respond', 'ApiController@respond');
+Route::get('webhook', 'ApiController@webhook');
+
+Route::get('webhook', 'ApiController@setWebHook');
+Route::post('442718912:AAFfm5yVq225XAsWMNRi7lifOVcRmFmsDjg/webhook', 'ApiController@setWebHook');
